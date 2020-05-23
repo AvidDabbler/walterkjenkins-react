8172784a7379b175c3db2603bc548e53f6403b70
AvidDabbler <walter.k.jenkins@gmail.com>
2020-05-23 08:14:40 -0500

    update to cors error/mapbox render

--- src/App.js
+++ src/App.js
@@ -3,33 +3,41 @@ import './App.css';
 import {mbToken} from './private';
 import mapboxgl from 'mapbox-gl';
 import Pbf from 'pbf';
-import {FeedMessage} from './gtfs-realtime.browser.proto.js';
+import { FeedMessage } from './gtfs-realtime.browser.proto.js';
+import cors_vehicles from './cors';
+// import express from 'express';
+// import cors from 'cors';
+// import bodyparser from 'body-parser';
+
+
+// !!! NOTES !!!
+// preprocessing .proto files and working with pbf -> https://gavinr.com/protocol-buffers-protobuf-browser/
+// processing cors errors -> https://daveceddia.com/access-control-allow-origin-cors-errors-in-react-express/
 
 
 
 mapboxgl.accessToken = mbToken();
 
 class BackgroundMap extends React.Component {
-  // Code from the next few steps will go here
-  constructor(props) {
-    super(props);
-    this.state = {
-    lng: -90.304,
-    lat: 38.674,
-    zoom: 10.9,
-    };
-	}
+	// Code from the next few steps will go here
+	constructor(props) {
+		super(props);
+		this.state = {
+			lng: -90.280318,
+			lat: 38.6447868,
+			zoom: 10.9,
+		};
+		}
 	
     componentDidMount() {
 
-		// https://grffe.com/proxy/proxy.php?
-		var pburl = "https://www.metrostlouis.org/RealTimeData/StlRealTimeVehicles.pb?cacheBust=";
 		let vehLocations;
+		var pburl = "https://www.metrostlouis.org/RealTimeData/StlRealTimeVehicles.pb?cacheBust=" + new Date().time;
 
-		// start of Gavin's code for processing protocol buffer information
+
 		const protobufUpdate = async () => {
-			const url = pburl +
-				new Date().getTime();
+			const url = cors_vehicles(pburl)
+			console.log(url);
 			let response = await fetch(url)
 			if (response.ok) {
 				// if HTTP-status is 200-299
@@ -42,21 +50,45 @@ class BackgroundMap extends React.Component {
 				console.error("error: ", response.status);
 			}
 		};
-		// end of Gavin's code
-		protobufUpdate()
-		.then(data => {
-				console.log(data);
-			}
-		)
 
+		
 
+		// start of Gavin's code for processing protocol buffer information
 		const map = new mapboxgl.Map({
 		container: this.mapContainer,
 		style: 'mapbox://styles/walterj/ckaer3czj1a6o1iqjjp5xy5dm',
 		center: [this.state.lng, this.state.lat],
 		zoom: this.state.zoom
 		});
-		}
+
+		protobufUpdate()
+			.then(data => {
+			console.log(data)
+			data.forEach(function(marker) {
+
+				// create a HTML element for each feature
+				var el = document.createElement('div');
+				el.className = 'marker';
+
+				let coord = [
+					marker.vehicle.position.longitude,
+					marker.vehicle.position.latitude,
+				];
+
+				console.log(coord)
+			  
+				// make a marker for each feature and add to the map
+				// styling -> https://docs.mapbox.com/mapbox-gl-js/example/data-driven-circle-colors/
+				new mapboxgl.Marker(el)
+					.setLngLat(coord)
+					.addTo(map);
+				
+				console.log()
+			  });
+		})
+	}
+	
+	
 
     render() {
 		
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import mapboxgl from'mapbox-gl';
 import * as p from 'private';
-import { BackgroundMap } from './App'
+import { BackgroundMap, Header } from './App'
 
 
 
 
-  ReactDOM.render(<BackgroundMap />, document.getElementById('root'));
+ReactDOM.render(
+  <BackgroundMap />,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
