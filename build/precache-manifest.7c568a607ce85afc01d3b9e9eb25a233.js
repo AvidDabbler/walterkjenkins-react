25140eae7ae7dee17ac6e39e2d0622ea5c6c248d
AvidDabbler <walter.k.jenkins@gmail.com>
2020-06-20 10:28:21 -0500

    yarn build

---
+++ build/precache-manifest.7c568a607ce85afc01d3b9e9eb25a233.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f7252cb8706a956834c280b0c0a20c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae54ab1fa638ce4de5a1",
+    "url": "/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "09eb02a5a9d455ee0c42",
+    "url": "/static/css/main.66e9979b.chunk.css"
+  },
+  {
+    "revision": "ae54ab1fa638ce4de5a1",
+    "url": "/static/js/2.47698feb.chunk.js"
+  },
+  {
+    "revision": "c5910952aae0e53539e67bcc9efbf67b",
+    "url": "/static/js/2.47698feb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09eb02a5a9d455ee0c42",
+    "url": "/static/js/main.8d52e053.chunk.js"
+  },
+  {
+    "revision": "9ce395a505a731f16821",
+    "url": "/static/js/runtime-main.14f2c381.js"
+  },
+  {
+    "revision": "5c2c034e78b766c9b8ae2c0696895213",
+    "url": "/static/media/data.5c2c034e.svg"
+  },
+  {
+    "revision": "7489f3283dd55c219d7e7abdeed908a1",
+    "url": "/static/media/email2.7489f328.svg"
+  },
+  {
+    "revision": "79aea739b577820806548d91b9c01e19",
+    "url": "/static/media/github.79aea739.svg"
+  },
+  {
+    "revision": "a59bbcc2d36ea8ede1c579fbf79e9ff7",
+    "url": "/static/media/javascript.a59bbcc2.svg"
+  },
+  {
+    "revision": "3cb3f2c16eb0007bccedcdc19b0dd713",
+    "url": "/static/media/linkedin2.3cb3f2c1.svg"
+  },
+  {
+    "revision": "75956deee0cf396d42b53404e35237f8",
+    "url": "/static/media/map.75956dee.svg"
+  },
+  {
+    "revision": "687ac050b48359b1d64943e6ac23fd9e",
+    "url": "/static/media/python.687ac050.svg"
+  },
+  {
+    "revision": "975c15b64bc04fba04e71aeb2dd157c4",
+    "url": "/static/media/tools.975c15b6.svg"
+  }
+]);
