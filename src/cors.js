f906569d35c0fe30de2b2d67740c7f97aa05f0a7
AvidDabbler <walter.k.jenkins@gmail.com>
2020-06-21 13:44:53 -0500

    removed ignore files

--- src/cors.js
+++
@@ -1,8 +0,0 @@
-const cors = (url) => {
-    return 'https://mysterious-cove-59901.herokuapp.com/' + url + new Date().time;
-}
-const cors_noDate = (url) => {
-    return 'https://mysterious-cove-59901.herokuapp.com/' + url;
-}
-
-export {cors,cors_noDate} ;
